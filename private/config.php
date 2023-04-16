d1ef208339ba6f5dd5108353ff8196a555b397a0
JPromi <jonasmpr05@gmail.com>
2023-04-16 20:49:48 +0200

    add default domain to config

--- private/config.php
+++ private/config.php
@@ -1,10 +1,11 @@
 <?php
 $domain  = array(
-    "intranet" =>    "intranet.pjugend.jpromi.com", //domain for intranet   #currently not in use
+    "intranet" =>   "intranet.pjugend.jpromi.com", //domain for intranet
     "web" =>        "pjugend.jpromi.com", //domain for public website       #currently not in use
     "auth" =>       "auth.pjugend.jpromi.com", //domain for login site
     "cdn" =>        "cdn.pjugend.jpromi.com", //doamin for cdn              #currently not in use
     "api" =>        "api.pjugend.jpromi.com", //doamin for cdn              #currently not in use
+    "default" =>    "pjugend.jpromi.com" //doamin for default
 );
 
 ?>
