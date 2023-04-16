d2c58a5f2a056802358d5e5670df6912ee402504
JPromi <jonasmpr05@gmail.com>
2023-04-16 19:29:45 +0200

    add domain config to config

---
+++ private/config.php
@@ -0,0 +1,10 @@
+<?php
+$domain  = array(
+    "intranet" =>    "intranet.pjugend.jpromi.com", //domain for intranet   #currently not in use
+    "web" =>        "pjugend.jpromi.com", //domain for public website       #currently not in use
+    "auth" =>       "auth.pjugend.jpromi.com", //domain for login site
+    "cdn" =>        "cdn.pjugend.jpromi.com", //doamin for cdn              #currently not in use
+    "api" =>        "api.pjugend.jpromi.com", //doamin for cdn              #currently not in use
+);
+
+?>
