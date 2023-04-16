c13722504f05e0485070d1eb6e25b2c622cf6ccf
JPromi <jonasmpr05@gmail.com>
2023-04-16 19:05:08 +0200

    add intranet db connection

---
+++ private/database/int.php
@@ -0,0 +1,13 @@
+<?php
+$db_HOST = "localhost";
+$db_USER = "pjugend";
+$db_PASSWD = "";
+$db_DATABASE = "pjugend_int";
+
+$con = mysqli_connect($db_HOST,$db_USER,$db_PASSWD,$db_DATABASE);
+$con_new = new mysqli($db_HOST,$db_USER,$db_PASSWD,$db_DATABASE);
+// Check connection
+if (mysqli_connect_errno()){
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
