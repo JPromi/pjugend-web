0e6f74ac89a7a61a4ea86f771462716f72d9f6bf
JPromi <jonasmpr05@gmail.com>
2023-04-16 20:50:26 +0200

    fix logout cookie

--- auth/logout.php
+++ auth/logout.php
@@ -1,10 +1,11 @@
 <?php
     include("../private/database/int.php");
+    include("../private/config.php");
     $session_hash = $_COOKIE["ARCADE_SESSION_ID"];
     mysqli_query($con, "DELETE FROM `session` WHERE cookie_hash = '$session_hash'");
 
     //delete cookie
-    setcookie("SESSION_ID", "", 0, "");
+    setcookie("SESSION_ID", "", 0, "", ".".$domain["default"]);
     
-    header("Location: /");
+    header("Location: https://".$domain["auth"]."/");
 ?>
