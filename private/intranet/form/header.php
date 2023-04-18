c36aacd8692e161584375ea0ea1ec476e6246b9d
JPromi <jonasmpr05@gmail.com>
2023-04-18 09:31:35 +0200

    add form header

---
+++ private/intranet/form/header.php
@@ -0,0 +1,30 @@
+<?php
+function top($title) {
+    global $dbSESSION_perm;
+?>
+<link rel="stylesheet" href="/form/css/header.css">
+<div class="top">
+
+    <div class="left">
+        <h4><a href="/form"><?php echo($title); ?></a></h4>
+    </div>
+
+    <div class="right">
+        <?php
+        if(in_array("event", $dbSESSION_perm)) {
+        ?>
+        <button title="Neue Veranstalltung" onclick="window.location.href=`/form/add`">
+            <span class="material-symbols-outlined">
+            add
+            </span>
+        </button>
+        <?php
+        };
+        ?>
+    </div>
+
+</div>
+
+<?php
+};
+?>
