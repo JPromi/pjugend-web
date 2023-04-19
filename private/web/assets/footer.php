816334e4f345b4add895ad83df844bda190baa9d
JPromi <jonasmpr05@gmail.com>
2023-04-19 08:57:19 +0200

    add public footer

---
+++ private/web/assets/footer.php
@@ -0,0 +1,27 @@
+<?php
+include($_SERVER["DOCUMENT_ROOT"]."/../private/config.php");
+?>
+
+<link rel="stylesheet" href="/css/style/footer.css">
+<footer>
+
+    <div class="main">
+
+        <div class="block">
+            <h4>Legacy</h4>
+            <a href="/impressum">Impressum</a>
+            <a href="/privacy">Datenschutz</a>
+        </div>
+
+        <div class="block">
+            <h4>Intern</h4>
+            <a href="https://<?php echo($domain["auth"]); ?>/">Login</a>
+        </div>
+
+    </div>
+
+    <div class="bottom">
+        <p>© <?php echo(date("Y")) ?> <a href="https://jpromi.com">JPromi.com</a></p>
+    </div>
+
+</footer>
