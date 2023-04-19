0856a8fbaf21ab682214bfb31fe1acee0bc52a18
JPromi <jonasmpr05@gmail.com>
2023-04-19 10:15:40 +0200

    add first version of impressum and privacy

---
+++ web/impressum.php
@@ -0,0 +1,48 @@
+<?php
+include '../private/config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Impressum - <?php echo($conf_title["web"]); ?></title>
+
+    <link rel="stylesheet" href="/css/style/style.css">
+    <link rel="stylesheet" href="css/impressum.css">
+</head>
+
+<?php
+include '../private/web/assets/nav.php';
+?>
+
+<body>
+   <section class="content">
+        <h2>Impressum</h2>
+
+        <div class="article">
+            <p>Adresse</p>
+            <p>PLZ Ort</p>
+            <p>Tel.: <a href="tel:+43123465789">+123456789</a></p>
+            <p>E-Mail.: <a href="mailto:ex@mp.le">ex@mp.le</a></p>
+        </div>
+
+        <div class="article">
+            <h4>Technische Umsetzung</h4>
+            <p>Jonas Prominzer</p>
+        </div>
+
+        <div class="article">
+            <h4>Bildmaterial</h4>
+            <p>Jonas Prominzer</p>
+        </div>
+   </section>
+</body>
+
+<?php
+include '../private/web/assets/footer.php';
+?>
+
+</html>
---
+++ web/privacy.php
@@ -0,0 +1,49 @@
+<?php
+include '../private/config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datenschutz - <?php echo($conf_title["web"]); ?></title>
+
+    <link rel="stylesheet" href="/css/style/style.css">
+    <link rel="stylesheet" href="css/impressum.css">
+</head>
+
+<?php
+include '../private/web/assets/nav.php';
+?>
+
+<body>
+   <section class="content">
+        <h2>Datenschutz</h2>
+
+        <div class="article">
+            <h4>1. Einverständniserklärung</h4>
+            <p>Diese Web-Site wird von der Perchtoldsdorf Pfarrjugend (Betreiber) verwaltet. Indem Sie auf sie zugreifen oder sie in einer anderen Form nutzen, erklären Sie sich mit den Bedingungen der Richtlinien für den Datenschutz wie unten aufgeführt einverstanden. Sollten Sie mit den Richtlinien nicht einverstanden sein, bitten wir Sie, diese Web-Site nicht abzurufen oder in irgendeiner anderen Form zu nutzen.</p>
+        </div>
+
+        <div class="article">
+            <h4>2. Umgang mit persönlichen Angaben</h4>
+            <p>
+                Wir wissen, dass Ihnen der sorgfältige Umgang mit Ihren persönlichen Informationen wichtig ist. Daher legt die Pfarrjugend Perchtoldsdorf größten Wert auf die Einhaltung der geltenden gesetzlichen Bestimmungen. Die Eingabe personenbezogener Daten, die Sie dem Betreiber der Site zukommen lassen, erfolgt völlig frei. Sollten Sie sich dafür entscheiden, kann der Betreiber u.U. von Ihnen die Zurverfügungstellung persönlicher Angaben (Name, E-Mail-Adresse, Postanschrift u. Ä.) verlangen. Bei der Aufgabe einer Bestellung werden Sie möglicherweise nach Ihrer Kreditkartennummer gefragt.<br><br>
+
+                Wenn Sie Ihre persönlichen Informationen an den Betreiber übermitteln, geben Sie Ihr Einverständnis, dass der Betreiber Ihr Kundenprofil speichern und weiterverarbeiten darf.<br><br>
+
+                Diese Angaben gehen beim Betreiber ein und werden dort gespeichert, um die Inanspruchnahme von Diensten der Web-Site zu ermöglichen. Eine Weitergabe Ihrer Daten an Dritte erfolgt nicht.<br><br>
+
+                Sobald Sie Ihre persönlichen Daten im Zusammenhang mit einer bestimmten Aktion auf der Web-Site an den Betreiber weitergeben, erklären Sie sich damit einverstanden, dass zum Zwecke der Abwicklung der Aktion und der Aktualisierung damit verbundener Datenbestände der Betreiber diese Daten übermitteln, speichern und verarbeiten darf.
+            </p>
+        </div>
+   </section>
+</body>
+
+<?php
+include '../private/web/assets/footer.php';
+?>
+
+</html>
