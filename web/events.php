228968d06ad3600d8a37f1b6920df5a3997f51a1
JPromi <jonasmpr05@gmail.com>
2023-04-27 09:18:11 +0200

    fix gramatic

--- web/events.php
+++ web/events.php
@@ -39,7 +39,7 @@ include '../private/database/int.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Veranstalltungen - <?php echo($conf_title["web"]); ?></title>
+    <title>Veranstaltungen - <?php echo($conf_title["web"]); ?></title>
 
     <link rel="stylesheet" href="/css/style/style.css">
     <link rel="stylesheet" href="css/events.css">
@@ -56,7 +56,7 @@ include '../private/web/assets/nav.php';
 
 <body>
     <div class="title">
-        <h4>Veranstalltungen</h4>
+        <h4>Veranstaltungen</h4>
     </div>
 
     <form class="search" method="post">
