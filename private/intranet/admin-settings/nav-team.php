be625d80ef2cd7160199f303d29d10b995d7d7b0
JPromi <jonasmpr05@gmail.com>
2023-05-02 08:50:13 +0200

    add team nav

---
+++ private/intranet/admin-settings/nav-team.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="/admin-settings/css/nav.css">
+<div class="left">
+    <div class="back">
+        <a href="/admin-settings">
+            <span class="material-symbols-outlined">
+            arrow_back
+            </span>
+            Zurück
+        </a>
+    </div>
+    <h1><?php echo($account["username"]);?></h1>
+    <div class="links">
+
+        <a href="/admin-settings/team">
+            <span class="material-symbols-outlined">
+            groups
+            </span>
+            Auswählen
+        </a>
+        
+    </div>
+</div>
