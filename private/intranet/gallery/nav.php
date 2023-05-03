17085b47a21db2e2a0c31b4b0e647254c6abfbac
JPromi <jonasmpr05@gmail.com>
2023-05-03 14:03:54 +0200

    gallery nav

--- private/intranet/gallery/nav.php
+++ private/intranet/gallery/nav.php
@@ -15,8 +15,22 @@
             $galleries = "SELECT * FROM gallery";
             $galleries = $con_public->query($galleries);
 
-            while ($gallery) {
-                # code...
+            while ($gallery = $galleries->fetch_assoc()) {
+                //get thumbnail
+                $pathGallery = $_SERVER["DOCUMENT_ROOT"].'/../cdn/gallery/'.$gallery["hash_id"].'/thumbnail/';
+                $galleryFolder = scandir ($pathGallery);
+                
+                if($galleryFolder[2] != "") {
+                    $thumbnail = 'https://'.$domain["cdn"].'/gallery/'.$gallery["hash_id"].'/thumbnail/'.$galleryFolder[2];
+                } else {
+                    $thumbnail = 'https://'.$domain["cdn"].'/gallery/placeholder/gallery.jpg';
+                }
+                echo '
+                <a href="/gallery/view?id='.$gallery["hash_id"].'">
+                    <img src="'.$thumbnail.'">
+                    <p>'.$gallery["title"].'</p>
+                </a>
+                ';
             }
             ?>
         </div>
