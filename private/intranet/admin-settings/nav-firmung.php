1f49f5c305c6a159cab970c532dab0df1d1f6555
JPromi <jonasmpr05@gmail.com>
2023-05-15 08:48:28 +0200

    add standard firmung nav

---
+++ private/intranet/admin-settings/nav-firmung.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" href="/admin-settings/css/nav.css">
+<div class="left">
+    <div class="back">
+        <a href="/admin-settings">
+            <span class="material-symbols-outlined">
+            arrow_back
+            </span>
+            Zurück
+        </a>
+    </div>
+    
+    <div class="links">
+        <a href="/admin-settings/firmung">
+            <span class="material-symbols-outlined">
+            info
+            </span>
+            Firmungen
+        </a>
+        <a href="/admin-settings/firmung/add">
+            <span class="material-symbols-outlined">
+            edit
+            </span>
+            Neue Firmung
+        </a>
+
+    </div>
+</div>
