7d3148cc891d2650d17c5d74bc512614506b5917
JPromi <jonasmpr05@gmail.com>
2023-05-15 08:28:40 +0200

    fix bug

--- private/database/firmung.php
+++ private/database/firmung.php
@@ -5,8 +5,8 @@ $db_PASSWD = "";
 $db_DATABASE = "pjugend_firmung";
 $db_PORT = 3306;
 
-$con_form = mysqli_connect($db_HOST,$db_USER,$db_PASSWD,$db_DATABASE,$db_PORT);
-$con_form_new = new mysqli($db_HOST,$db_USER,$db_PASSWD,$db_DATABASE,$db_PORT);
+$con_firmung = mysqli_connect($db_HOST,$db_USER,$db_PASSWD,$db_DATABASE,$db_PORT);
+$con_firmung_new = new mysqli($db_HOST,$db_USER,$db_PASSWD,$db_DATABASE,$db_PORT);
 // Check connection
 if (mysqli_connect_errno()){
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
