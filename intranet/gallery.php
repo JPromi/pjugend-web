ee908d9d7a0d1b5d725da007e098ffba9c9bf369
JPromi <jonasmpr05@gmail.com>
2023-05-22 07:52:51 +0200

    intranet gallery fix apache

--- intranet/gallery.php
+++ intranet/gallery.php
@@ -1,9 +1,9 @@
 <?php
 //include auth_session.php file on all user panel pages
-include("../private/session/auth_session.php");
+include($_SERVER["DOCUMENT_ROOT"]."/../private/session/auth_session.php");
 
-include("../private/database/public.php");
-include("../private/config.php");
+include($_SERVER["DOCUMENT_ROOT"]."/../private/database/public.php");
+include($_SERVER["DOCUMENT_ROOT"]."/../private/config.php");
 ?>
 
 <?php
@@ -25,30 +25,30 @@ if(in_array("gallery", $dbSESSION_perm) || in_array("jugendteam_admin", $dbSESSI
     <title>Galerie - <?php echo($conf_title["intranet"]); ?></title>
 
     <link rel="stylesheet" href="/css/style/style.css">
-    <link rel="stylesheet" href="css/gallery.css">
+    <link rel="stylesheet" href="/css/gallery.css">
     <link rel="stylesheet" href="/gallery/css/style.css">
         
     <?php
-    include '../private/favicon/main.php';
+    include $_SERVER["DOCUMENT_ROOT"].'/../private/favicon/main.php';
     ?>
 
 </head>
 <?php
 //include navigation bar
-include("../private/intranet/assets/nav.php")
+include($_SERVER["DOCUMENT_ROOT"]."/../private/intranet/assets/nav.php")
 ?>
 <body class="main" id="main">
 
 <div class="content">
 
         <?php
-        include '../private/intranet/gallery/header.php';
+        include $_SERVER["DOCUMENT_ROOT"].'/../private/intranet/gallery/header.php';
         top("Galerie");
         ?>
 
         <div class="gallery">
             <?php
-            include '../private/intranet/gallery/nav.php';
+            include $_SERVER["DOCUMENT_ROOT"].'/../private/intranet/gallery/nav.php';
             ?>
             <div class="middle">
 
@@ -61,7 +61,7 @@ include("../private/intranet/assets/nav.php")
 
 <?php
 //include scripts for bottom
-include("../private/intranet/assets/scripts-bottom.php")
+include($_SERVER["DOCUMENT_ROOT"]."/../private/intranet/assets/scripts-bottom.php")
 ?>
 
 </html>
