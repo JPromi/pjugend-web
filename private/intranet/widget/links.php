8d6408b4f984d38437d239a2154630a1b9bf0f01
JPromi <jonasmpr05@gmail.com>
2023-05-30 08:41:32 +0200

    add link widget

---
+++ private/intranet/widget/links.php
@@ -0,0 +1,24 @@
+<?php
+include $_SERVER["DOCUMENT_ROOT"].'/../private/config.php';
+?>
+<div class="widget w-links">
+    <div class="w-title">
+
+        <div class="w-icon">
+            <span class="material-symbols-outlined">
+            link
+            </span> 
+        </div>
+
+        <h1>Links</h1>
+    </div>
+    
+    <div class="w-li-links">
+        <a href="https://<?php echo($domain["web"]); ?>">
+            <span class="material-symbols-outlined">
+            public
+            </span>
+            Website
+        </a>
+    </div>
+</div>
