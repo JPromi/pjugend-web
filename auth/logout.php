b3de7471286ef137cc81ecf249ca013f46dbea38
JPromi <jonasmpr05@gmail.com>
2023-06-05 13:42:55 +0200

    add firmling logout

--- auth/logout.php
+++ auth/logout.php
@@ -1,11 +1,17 @@
 <?php
     include("../private/database/int.php");
+    include("../private/database/firmung.php");
     include("../private/config.php");
+
+    //intranet
     $session_hash = $_COOKIE["SESSION_ID"];
     mysqli_query($con, "DELETE FROM `session` WHERE cookie_hash = '$session_hash'");
-
-    //delete cookie
     setcookie("SESSION_ID", "", 0, "", ".".$domain["default"]);
+
+    $session_hash = $_COOKIE["SESSION_FIRMLING_ID"];
+    mysqli_query($con_firmung, "DELETE FROM `session` WHERE cookie_hash = '$session_hash'");
+    setcookie("SESSION_FIRMLING_ID", "", 0, "", ".".$domain["default"]);
+
     if(isset($_GET["direct"])) {
         $parameter = "?direct=".$_GET["direct"];
     }
