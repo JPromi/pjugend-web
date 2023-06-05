08e10055d1e4ed08105debb348a31ae398d11e78
JPromi <jonasmpr05@gmail.com>
2023-06-05 13:45:52 +0200

    edit redirect

--- private/session/firmung/auth_session.php
+++ private/session/firmung/auth_session.php
@@ -7,13 +7,7 @@ include($_SERVER["DOCUMENT_ROOT"]."/../private/config.php");
     if (empty($dbSESSION_firmling)) {
 
         //set redirect
-        $URL = $_SERVER["REQUEST_URI"];
-        $URL = substr($URL, 1);
-        //$URL = str_replace("?", "%3F", $URL);
-        //$URL = str_replace("=", "%3D", $URL);
-        //$URL = str_replace("/", "%2F", $URL);
-        header("Location: https://".$domain["auth"]."/?direct=".$URL);
+        header("Location: https://".$domain["auth"]."/?l=firmung");
         exit();
     }
 ?>
-
