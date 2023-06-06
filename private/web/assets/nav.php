1a583d3b4c4911317998bbdac7912e41f77e6534
JPromi <jonasmpr05@gmail.com>
2023-06-06 08:25:29 +0200

    add firming to nav

--- private/web/assets/nav.php
+++ private/web/assets/nav.php
@@ -1,6 +1,7 @@
 <?php
 include($_SERVER["DOCUMENT_ROOT"]."/../private/config.php");
 include($_SERVER["DOCUMENT_ROOT"]."/../private/session/get_session.php");
+include($_SERVER["DOCUMENT_ROOT"]."/../private/session/firmung/get_session.php");
 ?>
 
 <link rel="stylesheet" href="/css/style/nav.css">
@@ -45,8 +46,21 @@ include($_SERVER["DOCUMENT_ROOT"]."/../private/session/get_session.php");
                         $img_profile_path = "https://".$domain["cdn"]."/profile/placeholder/picture.jpg";
                     }
                     ?>
-                    <div class="profile" onclick="menu()">
+                    <div class="profile" onclick="menu('intranet')">
                         <img src="<?php echo($img_profile_path);?>">
+                    </div>
+
+                    
+                    <?php
+                }
+                ?>
+
+                <?php
+                // firmling
+                if(isset($dbSESSION_firmling) && !isset($dbSESSION)) {
+                    ?>
+                    <div class="profile profile-firmling" onclick="menu('firmling')">
+                        <img src="https://<?php echo($domain["cdn"]);?>/firmung/firmling/profilepicture">
                     </div>
 
                     
@@ -62,33 +76,58 @@ include($_SERVER["DOCUMENT_ROOT"]."/../private/session/get_session.php");
             // intranet
             if(isset($dbSESSION)) {
                 ?>
-                <div class="accountmenu hidden" id="accountMenu">
+                <div class="accountmenu hidden" id="menu-intranet">
                     <div class="info">
                         <img src="<?php echo($img_profile_path);?>">
                         <h2><?php echo($dbSESSION["username"]) ?></h2>
                     </div>
                     <div class="links">
+                        <div class="block">
+                            <a href="https://<?php echo($domain["intranet"]) ?>/">
+                                <span class="material-symbols-outlined">
+                                apps
+                                </span>
+                                Intranet
+                            </a>
 
-                        <a href="https://<?php echo($domain["intranet"]) ?>/">
-                            <span class="material-symbols-outlined">
-                            apps
-                            </span>
-                            Intranet
-                        </a>
+                            <a href="https://<?php echo($domain["intranet"]) ?>/notes">
+                                <span class="material-symbols-outlined">
+                                note
+                                </span>
+                                Notizen
+                            </a>
 
-                        <a href="https://<?php echo($domain["intranet"]) ?>/notes">
-                            <span class="material-symbols-outlined">
-                            note
-                            </span>
-                            Notizen
-                        </a>
+                            <a href="https://<?php echo($domain["intranet"]) ?>/settings">
+                                <span class="material-symbols-outlined">
+                                settings
+                                </span>
+                                Einstellungen
+                            </a>
+                        </div>
+                        
+                        <div class="block">
+                            <a href="/firmung/firmling">
+                                <span class="material-symbols-outlined">
+                                workspace_premium
+                                </span>
+                                Firmung
+                            </a>
 
-                        <a href="https://<?php echo($domain["intranet"]) ?>/settings">
-                            <span class="material-symbols-outlined">
-                            settings
-                            </span>
-                            Einstellungen
-                        </a>
+                            <a href="/firmung/firmling/event">
+                                <span class="material-symbols-outlined">
+                                schedule
+                                </span>
+                                Firm Aktionen
+                            </a>
+
+                            <a href="/firmung/firmling/settings">
+                                <span class="material-symbols-outlined">
+                                settings
+                                </span>
+                                Firm Einstellungen
+                            </a>
+                        </div>
+                        
                     </div>
                     <a href="https://<?php echo($domain["auth"]) ?>/logout?direct=public" class="logout">
                         <span class="material-symbols-outlined">
@@ -99,7 +138,57 @@ include($_SERVER["DOCUMENT_ROOT"]."/../private/session/get_session.php");
 
                     
                 </div>
-                <span onclick="menu()" class="fullscreenback hidden" id="accountmenuBack"></span>
+                <span onclick="menu('intranet')" class="fullscreenback hidden" id="menuBack-intranet"></span>
+                
+                <?php
+            }
+            ?>
+
+            <?php
+            // firmling
+            if(isset($dbSESSION_firmling) && !isset($dbSESSION)) {
+                ?>
+                <div class="accountmenu hidden" id="menu-firmling">
+                    <div class="info">
+                        <img src="https://<?php echo $domain["cdn"];?>/firmung/firmling/profilepicture">
+                        <h2><?php echo($dbSESSION["username"]) ?></h2>
+                    </div>
+                    <div class="links">
+
+                        <div class="block">
+                            <a href="/firmung/firmling">
+                                <span class="material-symbols-outlined">
+                                workspace_premium
+                                </span>
+                                Firmung
+                            </a>
+
+                            <a href="/firmung/firmling">
+                                <span class="material-symbols-outlined">
+                                schedule
+                                </span>
+                                Firm Aktionen
+                            </a>
+
+                            <a href="/firmung/firmling">
+                                <span class="material-symbols-outlined">
+                                settings
+                                </span>
+                                Firm Einstellungen
+                            </a>
+                        </div>
+
+                    </div>
+                    <a href="https://<?php echo($domain["auth"]) ?>/logout?direct=public" class="logout">
+                        <span class="material-symbols-outlined">
+                        logout
+                        </span>
+                        Logout
+                    </a>
+
+                    
+                </div>
+                <span onclick="menu('firmling')" class="fullscreenback hidden" id="menuBack-firmling"></span>
                 
                 <?php
             }
