f37cb17467bcbea1dd3859a4381da2970fb961c8
JPromi <jonasmpr05@gmail.com>
2023-06-27 10:48:47 +0200

    change get to request

--- api/auth/intranet/session.php
+++ api/auth/intranet/session.php
@@ -7,12 +7,12 @@ header('Access-Control-Allow-Origin: * ');
 header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Credentials: true');
 
-if(isset($_GET["SESSION_ID"])) {
-    $coockie_hash = stripslashes($_GET['SESSION_ID']);
+if(isset($_REQUEST["SESSION_ID"])) {
+    $coockie_hash = stripslashes($_REQUEST['SESSION_ID']);
     $coockie_hash = mysqli_real_escape_string($con, $coockie_hash);
 
     $session = $con->query("SELECT * FROM `session` WHERE `cookie_hash` = '$coockie_hash'")->fetch_assoc();
-    
+
     if(isset($session)) {
         echo json_encode($session);
     } else {
