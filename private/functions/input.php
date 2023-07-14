086b7e3f453f2f23ea0bb74b22d4ef523a2af522
JPromi <jonasmpr05@gmail.com>
2023-07-14 10:15:25 +0200

    add checkInput function

---
+++ private/functions/input.php
@@ -0,0 +1,16 @@
+<?php
+function checkInput($input) {
+    global $con;
+    $input = htmlspecialchars($input);
+    $input = stripslashes($input);
+    $input = mysqli_real_escape_string($con, $input);
+
+    if(!(empty($input))) {
+        $input = "'".$input."'";
+    } else {
+        $input = "NULL";
+    }
+
+    return $input;
+}
+?>
