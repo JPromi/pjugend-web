3386770fdea344c8178b704cf9f2e543caea18a1
JPromi <jonasmpr05@gmail.com>
2023-07-20 10:11:18 +0200

    fix perm

--- private/intranet/gallery/nav.php
+++ private/intranet/gallery/nav.php
@@ -12,27 +12,26 @@
 
         <div class="overview">
             <?php
-            $galleries = "SELECT * FROM gallery";
+            $user_id = $dbSESSION["user_id"];
+            $galleries = "SELECT * FROM gallery WHERE owner = '$user_id' OR id IN (SELECT gallery_id FROM gallery_permission WHERE user_id = '$user_id')";
             $galleries = $con_public->query($galleries);
 
             while ($gallery = $galleries->fetch_assoc()) {
-                if($gallery["owner"] == $dbSESSION["user_id"] || in_array("jugendteam_admin", $dbSESSION_perm) || in_array($dbSESSION["user_id"], explode(";", $gallery["user_edit"]))) {
-                    //get thumbnail
-                    $pathGallery = $_SERVER["DOCUMENT_ROOT"].'/../cdn/gallery/'.$gallery["hash_id"].'/thumbnail/';
-                    $galleryFolder = scandir ($pathGallery);
+                //get thumbnail
+                $pathGallery = $_SERVER["DOCUMENT_ROOT"].'/../cdn/gallery/'.$gallery["hash_id"].'/thumbnail/';
+                $galleryFolder = scandir ($pathGallery);
 
-                    if($galleryFolder[2] != "") {
-                        $thumbnail = 'https://'.$domain["cdn"].'/gallery/'.$gallery["hash_id"].'/thumbnail/'.$galleryFolder[2];
-                    } else {
-                        $thumbnail = 'https://'.$domain["cdn"].'/gallery/placeholder/gallery.jpg';
-                    }
-                    echo '
-                    <a href="/gallery/view?id='.$gallery["hash_id"].'">
-                        <img src="'.$thumbnail.'">
-                        <p>'.$gallery["title"].'</p>
-                    </a>
-                    ';
+                if($galleryFolder[2] != "") {
+                    $thumbnail = 'https://'.$domain["cdn"].'/gallery/'.$gallery["hash_id"].'/thumbnail/'.$galleryFolder[2];
+                } else {
+                    $thumbnail = 'https://'.$domain["cdn"].'/gallery/placeholder/gallery.jpg';
                 }
+                echo '
+                <a href="/gallery/view?id='.$gallery["hash_id"].'">
+                    <img src="'.$thumbnail.'">
+                    <p>'.$gallery["title"].'</p>
+                </a>
+                ';
                 
             }
             ?>
