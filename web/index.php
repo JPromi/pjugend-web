3acfe05b9948972a1f4666b3d719ef6c2842cb49
JPromi <jonasmpr05@gmail.com>
2023-08-24 09:10:22 +0200

    add news to home

--- web/index.php
+++ web/index.php
@@ -64,7 +64,7 @@ include '../private/web/assets/nav.php';
                         array_push($event_elements, array(
                             'date' => strtotime($eventCalendar["start"]),
                             'code' => '
-                            <div onclick="window.location.href=`/events/view?id='.$event['id'].'`" class="event">
+                            <a href="/events/view?id='.$event['id'].'" class="event">
                                 <div class="text">
                                     <h3>'.$event["title"].'</h3>
                                     <p>'.
@@ -78,7 +78,7 @@ include '../private/web/assets/nav.php';
                                 <div class="e-background">
                                     <img src="'.$event_img_path.'">
                                 </div>
-                            </div>
+                            </a>
                             ')
                         );
                     }
@@ -93,10 +93,10 @@ include '../private/web/assets/nav.php';
                 if($events_counter <= 4) {
                     for ($i=$events_counter; $i < $events_max; $i++) { 
                         echo '
-                        <div class="event placeholder" id="event' . $events_counter . '">
+                        <a class="event placeholder" id="event' . $events_counter . '">
                             <div class="e-background">
                             </div>
-                        </div>
+                        </a>
                         ';
                     }
                 }
@@ -108,17 +108,46 @@ include '../private/web/assets/nav.php';
         </div>
 
         <div class="news">
+            
             <h2>Neuigkeiten</h2>
             <div class="elements">
-                <div class="new">
-                    <h3>Text</h3>
-                </div>
-                <div class="new">
-                    <h3>Text</h3>
-                </div>
-                <div class="new">
-                    <h3>Text</h3>
-                </div>
+                <?php
+                // Letzen 4 - 6 News
+                // Titel, Bild, Teil der beschreibung
+
+                $news = $con_public->query("SELECT * FROM `news` ORDER BY `id` DESC");
+
+                while ($new  = $news->fetch_assoc()) {
+                    $new_img_path_root = $_SERVER["DOCUMENT_ROOT"].'/../cdn/news/'.$new["hash_id"].'.jpg';
+                    $new_img_path = 'https://'.$domain["cdn"].'/news/'.$new["hash_id"].'.jpg';
+
+                    echo '
+                    <a class="new" id="'.$new["hash_id"].'" href="news?id='.$new["hash_id"].'">
+                        <h3>'.$new["title"].'</h3>
+                        <div class="content">
+                    ';
+                    if(strlen($new["description"]) > 200) {
+                        echo '
+                            <p>'.substr(nl2br($new["description"]), 0, 200).'...</p>
+                        ';
+                    } else {
+                        echo '
+                            <p>'.nl2br($new["description"]).'</p>
+                        ';
+                    }
+
+                    if(file_exists($new_img_path_root)) {
+                        echo '
+                            <img src="'.$new_img_path.'">
+                        ';
+                    }
+                    
+                    echo '
+                        </div>
+                    </a>
+                    ';
+                }
+                ?>
             </div>
         </div>
     </div>
