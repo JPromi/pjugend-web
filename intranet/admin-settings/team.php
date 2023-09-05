f491214176416d00c84d6c2b30c04f104dfd29ba
JPromi <jonasmpr05@gmail.com>
2023-09-05 07:50:26 +0200

    fix perm select

--- intranet/admin-settings/team.php
+++ intranet/admin-settings/team.php
@@ -55,52 +55,48 @@ include($_SERVER["DOCUMENT_ROOT"]."/../private/intranet/assets/nav.php")
                     $allUsers = $con->query($allUsers);
 
                     //get goup permission
-                    $permissionGroup_jugendteam = "SELECT id FROM permissions_group WHERE perm = 'jugendteam'";
-                    $permissionGroup_jugendteam = $con->query($permissionGroup_jugendteam);
-                    $permissionGroup_jugendteam = $permissionGroup_jugendteam->fetch_assoc();
+
+                    $teamUsers = $con->query("SELECT user_id FROM accounts_permission_group WHERE group_id IN (SELECT id FROM permissions_group WHERE perm = 'jugendteam')")->fetch_all();
+                    $teamUsers = array_column($teamUsers, 0);
 
                     //go throught all users
-                    while ($user = $allUsers->fetch_assoc()) {
-                        //check if is in group
-                        if(in_array($permissionGroup_jugendteam["id"], explode(";", $user["permission_group"]))) {
+                    foreach ($teamUsers as $userID) {
+                        //selec team profile
+                        $user_id = $userID;
+                        $user = $con->query("SELECT * FROM accounts WHERE id = '$user_id'")->fetch_assoc();
+                        $teamEntry = "SELECT * FROM team WHERE user_id = '$user_id'";
+                        $teamEntry = $con_public->query($teamEntry);
+                        $teamEntry = $teamEntry->fetch_assoc();
 
-                            array_push($teamUsers, $user["id"]);
-
-                            //selec team profile
-                            $user_id = $user["id"];
-                            $teamEntry = "SELECT * FROM team WHERE user_id = '$user_id'";
-                            $teamEntry = $con_public->query($teamEntry);
-                            $teamEntry = $teamEntry->fetch_assoc();
-
-                            if (isset($teamEntry)) {
-                                $profile_image_root_path = $_SERVER["DOCUMENT_ROOT"]."/../cdn/profile/team/picture/im_p-".substr(md5($teamEntry["user_id"]), 0, 10).$teamEntry["user_id"].'-512.jpg';
-                                if(file_exists($profile_image_root_path)) {
-                                    $profile_image_path = "https://".$domain["cdn"].'/profile/team/picture/im_p-'.substr(md5($teamEntry["user_id"]), 0, 10).$teamEntry["user_id"].'-512.jpg';
-                                } else {
-                                    $profile_image_path = "https://".$domain["cdn"].'/profile/placeholder/picture.jpg';
-                                }
-                                
-                                echo '
-                                <div class="single" onclick="window.location.href=`/admin-settings/team/edit?id='.$user_id.'`">
-                                    <img src="'.$profile_image_path.'">
-                                    <h4>'.$teamEntry["name"].'</h4>
-    
-                                    <p class="focus sub">'.$teamEntry["focus"].'</p>
-                                    <p class="description">'.$teamEntry["description"].'</p>
-    
-                                </div>
-                                ';
+                        if (isset($teamEntry)) {
+                            $profile_image_root_path = $_SERVER["DOCUMENT_ROOT"]."/../cdn/profile/team/picture/im_p-".substr(md5($teamEntry["user_id"]), 0, 10).$teamEntry["user_id"].'-512.jpg';
+                            if(file_exists($profile_image_root_path)) {
+                                $profile_image_path = "https://".$domain["cdn"].'/profile/team/picture/im_p-'.substr(md5($teamEntry["user_id"]), 0, 10).$teamEntry["user_id"].'-512.jpg';
                             } else {
-                                echo '
-                                <div class="single new" onclick="window.location.href=`/admin-settings/team/edit?id='.$user_id.'`">
-                                    <img src="https://'.$domain["cdn"].'/profile/placeholder/picture.jpg">
-                                    <h4>'.$user["firstname"].' '.$user["lastname"].'</h4>    
-                                </div>
-                                ';
+                                $profile_image_path = "https://".$domain["cdn"].'/profile/placeholder/picture.jpg';
                             }
+                            
+                            echo '
+                            <div class="single" onclick="window.location.href=`/admin-settings/team/edit?id='.$user_id.'`">
+                                <img src="'.$profile_image_path.'">
+                                <h4>'.$teamEntry["name"].'</h4>
 
+                                <p class="focus sub">'.$teamEntry["focus"].'</p>
+                                <p class="description">'.$teamEntry["description"].'</p>
 
+                            </div>
+                            ';
+                        } else {
+                            $teamEntry = "SELECT * FROM team WHERE user_id = '$user_id'";
+                            echo '
+                            <div class="single new" onclick="window.location.href=`/admin-settings/team/edit?id='.$user_id.'`">
+                                <img src="https://'.$domain["cdn"].'/profile/placeholder/picture.jpg">
+                                <h4>'.$user["firstname"].' '.$user["lastname"].'</h4>    
+                            </div>
+                            ';
                         }
+
+
                     }
 
                     //all where account doesnt exist
